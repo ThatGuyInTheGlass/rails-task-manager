4e1850fb8b8dd82b3be107eb3c15d6dc1c0f24d2
Ross <ross.a@protonmail.ch>
2019-11-18 19:26:55 +0200

    Unfinished making it sexy

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # Read.all
+  get '/tasks', to: 'tasks#index', as: :tasks
+  # create new
+  get '/tasks/new', to: 'tasks#new', as: :tasks_new
+  post 'tasks', to: 'tasks#create'
+  # display one
+  get '/tasks/:id', to: 'tasks#show', as: :task
+  # update tasks
+  get '/tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id', to: 'tasks#update'
+  # delete task
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
